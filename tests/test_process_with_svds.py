1e1ac63b7253a76242223801400ed964b383c054
ckudera <32077257+ckudera@users.noreply.github.com>
2024-10-24 12:08:02 +0200

    add tests for deriving at register level

--- tests/test_process_with_svds.py
+++ tests/test_process_with_svds.py
@@ -2671,3 +2671,153 @@ class TestRegisterInheritanceViaDerivedFrom:
         assert device.peripherals[0].registers_clusters[1].fields[1].access == AccessType.WRITE_ONLY
         assert device.peripherals[0].registers_clusters[1].fields[1].write_constraint is not None
         assert device.peripherals[0].registers_clusters[1].fields[1].write_constraint.use_enumerated_values is True
+
+    def test_multiple_inheritance_backward_reference(self, get_processed_device_from_testfile: Callable[[str], Device]):
+        device = get_processed_device_from_testfile(
+            "register_inheritance_via_derivedfrom/multiple_inheritance_backward_reference.svd"
+        )
+
+        assert len(device.peripherals) == 1
+        assert len(device.peripherals[0].registers_clusters) == 3
+
+        assert isinstance(device.peripherals[0].registers_clusters[0], Register)
+        assert device.peripherals[0].registers_clusters[0].name == "RegisterA"
+        assert device.peripherals[0].registers_clusters[0].address_offset == 0x0
+        assert device.peripherals[0].registers_clusters[0].size == 32
+        assert len(device.peripherals[0].registers_clusters[0].fields) == 1
+        assert device.peripherals[0].registers_clusters[0].fields[0].name == "FieldA"
+        assert device.peripherals[0].registers_clusters[0].fields[0].lsb == 0
+        assert device.peripherals[0].registers_clusters[0].fields[0].msb == 2
+
+        assert isinstance(device.peripherals[0].registers_clusters[1], Register)
+        assert device.peripherals[0].registers_clusters[1].name == "RegisterB"
+        assert device.peripherals[0].registers_clusters[1].address_offset == 0x4
+        assert device.peripherals[0].registers_clusters[1].size == 32
+        assert len(device.peripherals[0].registers_clusters[1].fields) == 1
+        assert device.peripherals[0].registers_clusters[1].fields[0].name == "FieldA"
+        assert device.peripherals[0].registers_clusters[1].fields[0].lsb == 0
+        assert device.peripherals[0].registers_clusters[1].fields[0].msb == 2
+
+        assert isinstance(device.peripherals[0].registers_clusters[2], Register)
+        assert device.peripherals[0].registers_clusters[2].name == "RegisterC"
+        assert device.peripherals[0].registers_clusters[2].address_offset == 0x8
+        assert device.peripherals[0].registers_clusters[2].size == 32
+        assert len(device.peripherals[0].registers_clusters[2].fields) == 1
+        assert device.peripherals[0].registers_clusters[2].fields[0].name == "FieldA"
+        assert device.peripherals[0].registers_clusters[2].fields[0].lsb == 0
+        assert device.peripherals[0].registers_clusters[2].fields[0].msb == 2
+
+    def test_multiple_inheritance_forward_reference(self, get_processed_device_from_testfile: Callable[[str], Device]):
+        device = get_processed_device_from_testfile(
+            "register_inheritance_via_derivedfrom/multiple_inheritance_forward_reference.svd"
+        )
+
+        assert len(device.peripherals) == 1
+        assert len(device.peripherals[0].registers_clusters) == 3
+
+        assert isinstance(device.peripherals[0].registers_clusters[0], Register)
+        assert device.peripherals[0].registers_clusters[0].name == "RegisterA"
+        assert device.peripherals[0].registers_clusters[0].address_offset == 0x0
+        assert device.peripherals[0].registers_clusters[0].size == 32
+        assert len(device.peripherals[0].registers_clusters[0].fields) == 1
+        assert device.peripherals[0].registers_clusters[0].fields[0].name == "FieldA"
+        assert device.peripherals[0].registers_clusters[0].fields[0].lsb == 0
+        assert device.peripherals[0].registers_clusters[0].fields[0].msb == 2
+
+        assert isinstance(device.peripherals[0].registers_clusters[1], Register)
+        assert device.peripherals[0].registers_clusters[1].name == "RegisterB"
+        assert device.peripherals[0].registers_clusters[1].address_offset == 0x4
+        assert device.peripherals[0].registers_clusters[1].size == 32
+        assert len(device.peripherals[0].registers_clusters[1].fields) == 1
+        assert device.peripherals[0].registers_clusters[1].fields[0].name == "FieldA"
+        assert device.peripherals[0].registers_clusters[1].fields[0].lsb == 0
+        assert device.peripherals[0].registers_clusters[1].fields[0].msb == 2
+
+        assert isinstance(device.peripherals[0].registers_clusters[2], Register)
+        assert device.peripherals[0].registers_clusters[2].name == "RegisterC"
+        assert device.peripherals[0].registers_clusters[2].address_offset == 0x8
+        assert device.peripherals[0].registers_clusters[2].size == 32
+        assert len(device.peripherals[0].registers_clusters[2].fields) == 1
+        assert device.peripherals[0].registers_clusters[2].fields[0].name == "FieldA"
+        assert device.peripherals[0].registers_clusters[2].fields[0].lsb == 0
+        assert device.peripherals[0].registers_clusters[2].fields[0].msb == 2
+
+    @pytest.mark.xfail(strict=True, raises=ProcessException, reason="Circular inheritance is not supported")
+    def test_circular_inheritance(self, get_processed_device_from_testfile: Callable[[str], Device]):
+        get_processed_device_from_testfile("register_inheritance_via_derivedfrom/circular_inheritance.svd")
+
+    @pytest.mark.xfail(
+        strict=True,
+        raises=ProcessException,
+        reason="FieldA is already defined in RegisterA and cannot be inherited because it has the same name",
+    )
+    def test_field_inheritance_same_name(self, get_processed_device_from_testfile: Callable[[str], Device]):
+        get_processed_device_from_testfile("register_inheritance_via_derivedfrom/field_inheritance_same_name.svd")
+
+    @pytest.mark.xfail(
+        strict=True,
+        raises=ProcessException,
+        reason="FieldA overlaps with FieldB",
+    )
+    def test_field_inheritance_same_bit_range(self, get_processed_device_from_testfile: Callable[[str], Device]):
+        get_processed_device_from_testfile("register_inheritance_via_derivedfrom/field_inheritance_same_bit_range.svd")
+
+    @pytest.mark.xfail(
+        strict=True,
+        raises=ProcessException,
+        reason="FieldA overlaps with FieldB",
+    )
+    def test_field_inheritance_overlap_bit_range(self, get_processed_device_from_testfile: Callable[[str], Device]):
+        get_processed_device_from_testfile(
+            "register_inheritance_via_derivedfrom/field_inheritance_overlap_bit_range.svd"
+        )
+
+    def test_same_address(self, get_processed_device_from_testfile: Callable[[str], Device]):
+        with pytest.warns(ProcessWarning):
+            device = get_processed_device_from_testfile("register_inheritance_via_derivedfrom/same_address.svd")
+
+        assert len(device.peripherals) == 1
+        assert len(device.peripherals[0].registers_clusters) == 2
+
+        assert isinstance(device.peripherals[0].registers_clusters[0], Register)
+        assert device.peripherals[0].registers_clusters[0].name == "RegisterA"
+        assert device.peripherals[0].registers_clusters[0].address_offset == 0x0
+        assert device.peripherals[0].registers_clusters[0].size == 32
+        assert len(device.peripherals[0].registers_clusters[0].fields) == 1
+        assert device.peripherals[0].registers_clusters[0].fields[0].name == "FieldA"
+        assert device.peripherals[0].registers_clusters[0].fields[0].lsb == 0
+        assert device.peripherals[0].registers_clusters[0].fields[0].msb == 2
+
+        assert isinstance(device.peripherals[0].registers_clusters[1], Register)
+        assert device.peripherals[0].registers_clusters[1].name == "RegisterB"
+        assert device.peripherals[0].registers_clusters[1].address_offset == 0x0
+        assert device.peripherals[0].registers_clusters[1].size == 32
+        assert len(device.peripherals[0].registers_clusters[1].fields) == 1
+        assert device.peripherals[0].registers_clusters[1].fields[0].name == "FieldA"
+        assert device.peripherals[0].registers_clusters[1].fields[0].lsb == 0
+        assert device.peripherals[0].registers_clusters[1].fields[0].msb == 2
+
+    def test_register_overlap(self, get_processed_device_from_testfile: Callable[[str], Device]):
+        with pytest.warns(ProcessWarning):
+            device = get_processed_device_from_testfile("register_inheritance_via_derivedfrom/register_overlap.svd")
+
+        assert len(device.peripherals) == 1
+        assert len(device.peripherals[0].registers_clusters) == 2
+
+        assert isinstance(device.peripherals[0].registers_clusters[0], Register)
+        assert device.peripherals[0].registers_clusters[0].name == "RegisterA"
+        assert device.peripherals[0].registers_clusters[0].address_offset == 0x0
+        assert device.peripherals[0].registers_clusters[0].size == 32
+        assert len(device.peripherals[0].registers_clusters[0].fields) == 1
+        assert device.peripherals[0].registers_clusters[0].fields[0].name == "FieldA"
+        assert device.peripherals[0].registers_clusters[0].fields[0].lsb == 0
+        assert device.peripherals[0].registers_clusters[0].fields[0].msb == 2
+
+        assert isinstance(device.peripherals[0].registers_clusters[1], Register)
+        assert device.peripherals[0].registers_clusters[1].name == "RegisterB"
+        assert device.peripherals[0].registers_clusters[1].address_offset == 0x2
+        assert device.peripherals[0].registers_clusters[1].size == 16
+        assert len(device.peripherals[0].registers_clusters[1].fields) == 1
+        assert device.peripherals[0].registers_clusters[1].fields[0].name == "FieldA"
+        assert device.peripherals[0].registers_clusters[1].fields[0].lsb == 0
+        assert device.peripherals[0].registers_clusters[1].fields[0].msb == 2
