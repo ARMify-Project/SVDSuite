7bcb1576c60f73cb14e8fccb3ff98454ba87e7c8
ckudera <32077257+ckudera@users.noreply.github.com>
2025-03-11 22:04:53 +0100

    update main init.py

--- src/svdsuite/__init__.py
+++ src/svdsuite/__init__.py
@@ -1,5 +1,6 @@
-from svdsuite.parse import Parser, ParserException
-from svdsuite.process import Process, ProcessException
+from svdsuite.parse import Parser
+from svdsuite.util.parser_exception_warning import ParserException, ParserWarning
+from svdsuite.process import Process, ProcessException, ProcessWarning
 from svdsuite.validate import Validator, ValidatorException, SVDSchemaVersion
 from svdsuite.serialize import Serializer
 from svdsuite.map import PeripheralRegisterMap
@@ -7,8 +8,10 @@ from svdsuite.map import PeripheralRegisterMap
 __all__ = [
     "Parser",
     "ParserException",
+    "ParserWarning",
     "Process",
     "ProcessException",
+    "ProcessWarning",
     "Validator",
     "ValidatorException",
     "SVDSchemaVersion",
